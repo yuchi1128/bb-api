3d930e7819dc46ede2fca9fd0edf2a7c2a18c178
yuchi1128 <yukiya20021128@icloud.com>
2024-11-29 23:37:55 +0900

    todoリストを作成できるようにた

---
+++ app/models/todos.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"log"
+	"time"
+)
+
+type Todo struct {
+	ID        int
+	Content   string
+	UserID    int
+	CreatedAt time.Time
+}
+
+func (u *User) CreateTodo(content string) (err error) {
+	cmd := `INSERT INTO todos (content, user_id, created_at) VALUES (?, ?, ?)`
+	_, err = Db.Exec(cmd, content, u.ID, time.Now())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
